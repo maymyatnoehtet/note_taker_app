e99d65491375d9f21b656b7cbc571d4824972a42
maymyatnoehtet <may.mn.htet@gmail.com>
2023-05-12 15:44:45 +1000

    add delete api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,7 @@ const api_router = express.Router();
 // API endpoint to get notes
 api_router.get('/api/notes', async (req, res) => {
   try {
+    // Read all the notes
     const notesJSON = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
     res.json(notesJSON);
   } catch (error) {
@@ -22,19 +23,41 @@ api_router.post('/api/notes', async (req, res) => {
   try {
     const notesJSON = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
     const newNote = {
-      // to create unique identifier for each note
+      // Create unique identifier for each note
       id: uuidv4(),
       title: req.body.title,
       text: req.body.text
     };
+    // Add the new note to existing JSON notes
     notesJSON.push(newNote);
     fs.writeFileSync("db/db.json", JSON.stringify(notesJSON));
+    // Show the new note
     res.json(newNote);
   } catch (error) {
     console.error(error);
     res.status(500).send('Server Error');
   }
 });
+
+// API endpoint to delete a note given id
+api_router.delete('/api/notes/:id', async (req, res) => {
+  try {
+    const notesJSON = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    // Remove the note with given ID from the notes array
+    const newNotes = notesJSON.filter((note) => { 
+      return note.id !== req.params.id;
+    });
+    // Write the updated notes array to the file
+    fs.writeFile("db/db.json", JSON.stringify(newNotes), (err) => {
+      if (err) throw err;
+      res.json("Note deleted.");
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server Error");
+  }
+});
+
 
 
 module.exports = api_router;
